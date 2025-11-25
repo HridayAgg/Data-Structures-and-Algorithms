5780860b1bf95b1d243883306ac9a0ba3d5e97c7
Hriday Aggarwal <9riday@gmail.com>
2025-11-25 16:58:02 +0530

    L8-Basics/

---
+++ L8-Basics/AlphabetPattern.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    char c ='A';
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n-i; j++)
+        {
+            cout<<c<<' ';
+            c++;
+        }
+        for (int j = 0; j < n-i; j++)
+        {
+            c--;
+            cout<<c<<' ';
+        }
+        cout<<endl;
+        
+    }
+    
+    return 0;
+}
